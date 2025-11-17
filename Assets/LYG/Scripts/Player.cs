e09ac3fa8be096cc33d1c66b7852ffacda8e8674
SUBcharacter <farg557@gmail.com>
2025-11-17 23:45:25 +0900

    기본 이동 무브셋, 조준 상태 작업 완료

--- Assets/LYG/Scripts/Player.cs
+++ Assets/LYG/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Transform arm;
     [SerializeField] Rigidbody2D rigid;
+    [SerializeField] SpriteRenderer ren;
     [SerializeField] Vector2 moveVec;
     [SerializeField] Vector2 mousePos;
     [SerializeField] LayerMask groundMask;
@@ -13,21 +15,56 @@ public class Player : MonoBehaviour
     [SerializeField] float jumpForce;
 
     [SerializeField] bool isGround;
+    [SerializeField] bool canAirJump;
+    [SerializeField] bool aiming;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        ren = GetComponent<SpriteRenderer>();
+        Cursor.lockState = CursorLockMode.Confined;
+        arm.gameObject.SetActive(false);
     }
 
     private void Update()
     {
-        
+        RotateArm();
+        SpriteControl();
     }
 
     private void FixedUpdate()
     {
         Move();
         GroundCheck();
+        
+    }
+
+    void SpriteControl()
+    {
+        if(aiming)
+        {
+            Vector2 dir = (mousePos - (Vector2)arm.position).normalized;
+
+            if(dir.x <0)
+            {
+                ren.flipX = true;
+            }
+            else if(dir.x >0)
+            {
+                ren.flipX = false;
+            }
+        }
+        else
+        {
+            if(moveVec.x < 0)
+            {
+                ren.flipX = true;
+            }
+            else if(moveVec.x > 0)
+            {
+                ren.flipX = false;
+            }
+        }
     }
 
     void Move()
@@ -46,6 +83,23 @@ public class Player : MonoBehaviour
         Vector2 bottomCenter = (Vector2)transform.position + new Vector2(0, bottomY);
 
         isGround = Physics2D.CircleCast(bottomCenter, radius, Vector2.down, 0.05f, groundMask);
+
+        if(isGround)
+        {
+            canAirJump = isGround;
+        }
+    }
+
+    void RotateArm()
+    {
+        if (!aiming)
+            return;
+
+        Vector2 dir = (mousePos - (Vector2)arm.position).normalized;
+
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        arm.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     #region Input System
@@ -63,18 +117,40 @@ public class Player : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (!isGround)
-            return;
-
         if(context.started)
         {
-            rigid.linearVelocityY = jumpForce;
+            if(isGround)
+            {
+                rigid.linearVelocityY = jumpForce;
+            }
+            else if(canAirJump)
+            {
+                rigid.linearVelocityY = jumpForce;
+                canAirJump = false;
+            }
+            
         }
     }
 
     public void OnMouse(InputAction.CallbackContext context)
     {
-        mousePos = context.ReadValue<Vector2>();
+        Vector2 inputVec = context.ReadValue<Vector2>();
+
+        mousePos = Camera.main.ScreenToWorldPoint(inputVec);
+    }
+
+    public void OnSubAttack(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            aiming = true;
+            arm.gameObject.SetActive(aiming);
+        }
+        else if(context.canceled)
+        {
+            aiming = false;
+            arm.gameObject.SetActive(aiming);
+        }
     }
 
     #endregion
