4d0dd3c08205aa5b21a4f88814a9253ee768e096
gydud0819 <pstar0819@naver.com>
2025-11-29 23:55:47 +0900

    (2025-11-29) 원거리 적 구현 중간 점검
    - 버그 다수 발견 및 수정 예정

--- Assets/PHY/Scripts/Enemy/EnemyBullet.cs
+++ Assets/PHY/Scripts/Enemy/EnemyBullet.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class EnemyBullet : Bullet
 {
-
     public void Fire(Vector3 startPos, Vector3 targetPos)
     {
         Vector2 direction = (targetPos - startPos).normalized;
--- Assets/PHY/Scripts/Enemy/LongDiEnemy.cs
+++ Assets/PHY/Scripts/Enemy/LongDiEnemy.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class LongDiEnemy : MonoBehaviour
 {
+    [Header("Enemy 체력관련 변수")]
+    [SerializeField] private int maxHits = 4;
+    private int currentHits = 0;
+
     [Header("Enemy 이동 관련 변수")]
     [SerializeField] private float moveSpeed = 1.5f;
     [SerializeField] private float moveDistance = 2.5f;
@@ -10,7 +14,7 @@ public class LongDiEnemy : MonoBehaviour
 
 
     [Header("Enemy 총알 발사 관련 변수")]
-    [SerializeField] private EnemyBullet enemyBullet;
+    [SerializeField] private GameObject enemyBulletPrefab;
     [SerializeField] private Transform firePoint;
 
     // 내부 컴포넌트
@@ -23,6 +27,11 @@ public class LongDiEnemy : MonoBehaviour
     [SerializeField] private float fireCooldown = 1f;
     private float fireTimer = 0f;
 
+    [Header("애니메이션 관련 변수")]
+    private Animator animator;
+    [SerializeField] private bool isAiming = false;
+    [SerializeField] private bool isFiring = false;
+    private float ReadytoFireTime = 0.5f;  // 조준으로 넘어갈 때 준비 시간
 
     private void Awake()
     {
@@ -30,11 +39,15 @@ public class LongDiEnemy : MonoBehaviour
 
         spriteRenderer = GetComponent<SpriteRenderer>();
 
+        animator = GetComponent<Animator>();
+
         startPos = transform.position;
     }
 
     private void Update()
     {
+        Debug.Log("aim: " + isAiming);
+
         fireTimer += Time.deltaTime;
         TryFire();
     }
@@ -47,6 +60,12 @@ public class LongDiEnemy : MonoBehaviour
     // 에너미 이동 함수
     private void Move()
     {
+        if (isAiming || isFiring)
+        {
+            rigid.linearVelocity = Vector2.zero;
+            return;
+        }
+
         float moveDir = isMovingRight ? 1f : -1f;
 
         rigid.linearVelocity = new Vector2(moveDir * moveSpeed, rigid.linearVelocity.y);
@@ -65,26 +84,65 @@ public class LongDiEnemy : MonoBehaviour
 
     void TryFire()
     {
-        // 플레이어가 시야 범위안에 있는지 확인
-        if (!detectionRange.isPlayerInRange) return;
+        //플레이어가 시야 범위 안에 있는지 확인
+        if (!detectionRange.isPlayerInRange)
+        {
+            isAiming = false;
+            isFiring = false;
+            return;
+        }
 
-        // 플레이어가 공격 범위안에 있는지 확인
-        if (!attackRange.isPlayerInAttackRange) return;
+        // 공격 범위 안에 플레이어가 있는지 확인
+        if (!attackRange.isPlayerInAttackRange)
+        {
+            isAiming = false;
+            isFiring = false;
+            return;
+        }
 
         if (fireTimer < fireCooldown) return;
 
-        fireTimer = 0f;
+        isAiming = true;
+        isFiring = true;
 
-        Fire();
+        animator.SetTrigger("isAttack");
+        // 총 쏘는 동안 제자리
+        rigid.linearVelocity = Vector2.zero;
+
+        Debug.Log("EnemtBullet launched attacked.");
+
+        fireTimer = 0f;
     }
 
     void Fire()
     {
-        Vector3 targetPos = attackRange.target.position;
+        // 플레이어의 마지막 위치를 기준으로 불렛 발사
+        Vector3 lastTargetPos = attackRange.target.position;
 
-        EnemyBullet E = Instantiate(enemyBullet);
+        // 이건 총알이 제대로 날아가는지 테스트한 코드
+        //Vector3 targetPos = attackRange.target.position;
 
-        E.Fire(firePoint.position, targetPos);
+        GameObject enemy = Instantiate(enemyBulletPrefab, firePoint.position, Quaternion.identity);
+
+        EnemyBullet enemyBullet = enemy.GetComponent<EnemyBullet>();
+
+        enemyBullet.Fire(firePoint.position, lastTargetPos);
+
+        isAiming = false;
+    }
+
+    void TakeHit()
+    {
+        currentHits++;
+
+        if ((currentHits >= maxHits))
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
 
