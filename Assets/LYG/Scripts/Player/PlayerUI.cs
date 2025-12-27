355ba33f213b2b8c17ec2beb70ab8f6b1a800b1e
SUBcharacter <farg557@gmail.com>
2025-12-27 16:43:26 +0900

    병합 준비

--- Assets/LYG/Scripts/Player/PlayerUI.cs
+++ Assets/LYG/Scripts/Player/PlayerUI.cs
@@ -8,8 +8,9 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] Player player;
 
     [SerializeField] Image targetCrossHairPrefab;
-    [SerializeField] GameObject meleeSkills;
-    [SerializeField] GameObject rangeSkills;
+    //[SerializeField] GameObject skills;
+    //[SerializeField] GameObject meleeSkills;
+    //[SerializeField] GameObject rangeSkills;
     [SerializeField] Image chargeAttackIcon;
     [SerializeField] Image flashAttackIcon;
     [SerializeField] Image phantomBladeIcon;
@@ -32,6 +33,7 @@ public class PlayerUI : MonoBehaviour
             targetCrossHair.Add(instance);
             targetCrossHair[i].gameObject.SetActive(false);
         }
+        
     }
 
     private void Update()
@@ -44,22 +46,92 @@ public class PlayerUI : MonoBehaviour
         bulletCount.text = player.BulletCount.ToString();
         stamina.fillAmount = player.Stamina / player.Stats.maxStamina;
         overFlowEnergy.fillAmount = player.OverFlowEnergy / player.Stats.maxOverFlowEnergy;
-        switch(player.CrWp)
+        //switch(player.CrWp)
+        //{
+        //    case WeaponState.Melee:
+        //        if (rangeSkills.activeSelf)
+        //            rangeSkills.SetActive(false);
+        //        meleeSkills.SetActive(true);
+        //        chargeAttackIcon.fillAmount = player.SkMn.ChargeAttackTimer / player.SkMn.ChargeAttackStat.coolTime;
+        //        ChargeAttackUI();
+        //        flashAttackIcon.fillAmount = player.SkMn.FlashAttackTimer / player.SkMn.FlashAttackStat.coolTime;
+        //        break;
+        //    case WeaponState.Ranged:
+        //        if (meleeSkills.activeSelf)
+        //            meleeSkills.SetActive(false);
+        //        rangeSkills.SetActive(true);
+        //        phantomBladeIcon.fillAmount = player.SkMn.PhantomBladeTimer / player.SkMn.PhantomBladeStat.coolTime;
+        //        autoTargetingIcon.fillAmount = player.SkMn.AutoTargetingTimer / player.SkMn.AutoTargetingStat.coolTime;
+        //        break;
+        //}
+        PhantomBladeUI();
+        ChargeAttackUI();
+        FlashAttackUI();
+        AutoTargetingUI();
+    }
+
+    void PhantomBladeUI()
+    {
+        if(player.SkMn.PhantomBladeUsable)
         {
-            case WeaponState.Melee:
-                if (rangeSkills.activeSelf)
-                    rangeSkills.SetActive(false);
-                meleeSkills.SetActive(true);
-                chargeAttackIcon.fillAmount = player.SkMn.ChargeAttackTimer / player.SkMn.ChargeAttackStat.coolTime;
-                flashAttackIcon.fillAmount = player.SkMn.FlashAttackTimer / player.SkMn.FlashAttackStat.coolTime;
-                break;
-            case WeaponState.Ranged:
-                if (meleeSkills.activeSelf)
-                    meleeSkills.SetActive(false);
-                rangeSkills.SetActive(true);
-                phantomBladeIcon.fillAmount = player.SkMn.PhantomBladeTimer / player.SkMn.PhantomBladeStat.coolTime;
-                autoTargetingIcon.fillAmount = player.SkMn.AutoTargetingTimer / player.SkMn.AutoTargetingStat.coolTime;
-                break;
+            phantomBladeIcon.transform.SetAsLastSibling();
+            phantomBladeIcon.gameObject.SetActive(false);
+        }
+        else
+        {
+            phantomBladeIcon.gameObject.SetActive(true);
+            phantomBladeIcon.fillAmount = player.SkMn.PhantomBladeTimer / player.SkMn.PhantomBladeStat.coolTime;
+        }
+    }
+
+    void ChargeAttackUI()
+    {
+        if(player.SkMn.ChargeAttackUsable)
+        {
+            Debug.Log("비활성화");
+            chargeAttackIcon.transform.SetAsLastSibling();
+            chargeAttackIcon.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("활성화");
+
+            chargeAttackIcon.gameObject.SetActive(true);
+            chargeAttackIcon.fillAmount = player.SkMn.ChargeAttackTimer / player.SkMn.ChargeAttackStat.coolTime;
+        }
+    }
+
+    void FlashAttackUI()
+    {
+        if (player.SkMn.FlashAttackUsable)
+        {
+            Debug.Log("비활성화");
+            flashAttackIcon.transform.SetAsLastSibling();
+            flashAttackIcon.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("활성화");
+
+            flashAttackIcon.gameObject.SetActive(true);
+            flashAttackIcon.fillAmount = player.SkMn.FlashAttackTimer / player.SkMn.FlashAttackStat.coolTime;
+        }
+    }
+
+    void AutoTargetingUI()
+    {
+        if (player.SkMn.AutoTargetingUsable)
+        {
+            Debug.Log("비활성화");
+            autoTargetingIcon.transform.SetAsLastSibling();
+            autoTargetingIcon.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("활성화");
+
+            autoTargetingIcon.gameObject.SetActive(true);
+            autoTargetingIcon.fillAmount = player.SkMn.AutoTargetingTimer / player.SkMn.AutoTargetingStat.coolTime;
         }
     }
 
